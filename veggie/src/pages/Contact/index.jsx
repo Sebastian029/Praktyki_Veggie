d348d92b430bee661cc1915b30811262e7a23a8f
Blazej <jakubczyk.blazej@gmail.com>
2024-07-03 13:17:53 +0200

    Contact final

--- veggie/src/pages/Contact/index.jsx
+++ veggie/src/pages/Contact/index.jsx
@@ -3,12 +3,44 @@ import background from '../../assets/background3.png';
 import decoration from '../../assets/decoration.png';
 import facebook from '../../assets/facebook.png';
 import instagram from '../../assets/instagram.png';
+import React, { useState, useEffect } from 'react';
 
 function index() {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form data:', formData);
+    };
+
+    const backgroundImageStyle = screenWidth <= 320 ? { backgroundImage: 'none' } : { backgroundImage: `url(${background})` };
+    
     return (
         <div 
             className={styles.main}
-            style={{ backgroundImage: `url(${background})` }}
+            style={ backgroundImageStyle}
         >
             <div className={styles.contact}>
                 <h1 className={styles.header}>
@@ -28,15 +60,36 @@ function index() {
                         <p className={styles.text}>+48 202-555-0114</p>
                     </div>
                 </div>
-                <div className={styles.contactForm}>
+                <form className={styles.contactForm} onSubmit={handleSubmit}>
                     <h2 className={styles.header2}>CONTACT FORM</h2>
                     <div className={styles.inputs}>
-                        <input className={styles.input} type='text' placeholder='Name'/>
-                        <input className={styles.input} type='email' placeholder='Email'/>
-                        <input className={styles.message} type='text' placeholder='Mesage'/>
+                        <input 
+                            className={styles.input} 
+                            type='text' 
+                            placeholder='Name'
+                            name='name'
+                            value={formData.name}
+                            onChange={handleInputChange}
+                        />
+                        <input 
+                            className={styles.input} 
+                            type='email' 
+                            name='email'
+                            placeholder='Email'
+                            value={formData.email}
+                            onChange={handleInputChange}
+                        />
+                        <input 
+                            className={styles.message} 
+                            type='text' 
+                            name='message'
+                            placeholder='Message'
+                            value={formData.message}
+                            onChange={handleInputChange}
+                        />
                     </div>
-                    <button className={styles.button}>Send</button>
-                </div>
+                    <button className={styles.button} type='submit'>Send</button>
+                </form>
             </div>
             <div className={styles.socials}>
                 <img className={styles.icon} src={facebook} alt="" />
